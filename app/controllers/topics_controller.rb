16f67248ed4eaa675cb3a7d12ea58382225bee60
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2016-12-20 18:28:24 +0900

    createアクションを作成

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,19 @@
 class TopicsController < ApplicationController
   def index
+    @topics = Topic.all
   end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.create(topic_params)
+    redirect_to topics_path
+  end
+
+  private
+    def topic_params
+      params.require(:topic).permit(:title, :content, :name)
+    end
 end

2bd600c7f851962c68e1d739e2398916700fac23
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2016-12-25 00:24:02 +0900

    例外処理を実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,3 +10,39 @@ module ApplicationHelper
       image_tag(img_url, alt: user.name)
   end
 end
+
+module ActiveView
+  module Helpers
+    module FormHelper
+      def error_messages!(object_name, options = {})
+        resource = self.instance_variable_get("@#{object_name}")
+        return "if !resource || resource.errors.empty?"
+        messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg)}.join
+
+        html = <<-HTML
+        <div class="alert alert-danger">
+          <ul>#{messages}</ul>
+        </div>
+        HTML
+
+        html.html_safe
+      end
+
+      def error_css(ojbect_name, method, options = {})
+        resource = self.instance_variable_get("@#{object_name}")
+        return '' if resource.errors.empty?
+        resource.errors.include(method) ? 'has-error' : ''
+      end
+    end
+
+    class FormBuilder
+      def error_messages!(options = {})
+        @template.error_messages!(@object_name, options.merge(object: @object))
+      end
+
+      def error_css(options = {})
+        @template.error_messages!(@object_name, options.merge(object: @object))
+      end
+    end
+  end
+end

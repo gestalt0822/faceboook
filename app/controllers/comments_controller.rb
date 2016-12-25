fd41294f2b0a48f3c4240e5ae4f3414ff84e320b
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2016-12-25 16:31:42 +0900

    コメント削除機能(ajax)を実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,6 +13,20 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @topic = @comment.topic
+
+    respond_to do |format|
+      if @comment.destroy
+        format.html{ redirect_to topic_path(@topic), notice: 'コメントを削除しました！'}
+        format.js {render :index}
+      else
+        format.html {render :new}
+      end
+    end
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:topic_id, :content)

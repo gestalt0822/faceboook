923b5611f83caf534a4cc20a803d007c70bbeb72
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2016-12-28 15:22:26 +0900

    ●●.html.erbのファイルのコードをリファクタ

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |n|
+  name = Faker::Pokemon.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               )
+end
+
+10.times do |n|
+  title = Faker::Color.color_name
+  content = Faker::Lorem.sentence
+  Topic.create!(title: title,
+               content: content,
+               user_id: 37
+               )
+end

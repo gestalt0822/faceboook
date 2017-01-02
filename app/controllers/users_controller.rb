86b0b808ec64f5b6de7b0bdaa6a2f9f005aab4e7
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2017-01-02 18:31:12 +0900

    ユーザー詳細ページを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @mentors = @user.mentors
     @fans = @user.fans
+    @post_number = @user.topics.count
+    @comment_number = @user.comments.count
   end
 end
